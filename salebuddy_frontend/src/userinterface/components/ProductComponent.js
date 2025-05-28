c374f8a9efca0e323b36fca4b6318d905405eef2
Rohan2519Y <rohankumar17645@gmail.com>
2025-05-28 11:59:48 +0530

    adding the product component

---
+++ salebuddy_frontend/src/userinterface/components/ProductComponent.js
@@ -0,0 +1,7 @@
+export default function ProductComponent(){
+    return (
+        <div style={{height:300,width:'100%',background:' #191919'}}>
+
+        </div>
+    )
+}
--- salebuddy_frontend/src/userinterface/screens/Home.js
+++ salebuddy_frontend/src/userinterface/screens/Home.js
@@ -2,13 +2,17 @@ import Header from "../components/Header"
 import MainSlider from "../components/MainSlider"
 import Footer from "../components/Footer"
 import BrandSlider from "../components/BrandSlider"
+import ProductComponent from "../components/ProductComponent"
 export default function Home() {
     return (<div style={{ width: '100%', display: 'flex', flexDirection: 'column' }} >
         <Header />
         <div style={{ width: '100%',height:'100%',overflowX: "hidden",background:' #191919'}}>
             <MainSlider />
         </div>
-        <div style={{width:'100%',height:'100%'}}>
+        <div>
+        <ProductComponent />
+        </div>
+        <div >
         <BrandSlider />
         </div>
         <div>
