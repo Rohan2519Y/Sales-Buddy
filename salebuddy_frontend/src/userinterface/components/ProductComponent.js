f8146cb7a80155b37d1af14d391e5d6a0436ce55
Rohan2519Y <rohankumar17645@gmail.com>
2025-05-30 12:18:44 +0530

    Update ProductComponent.js

--- salebuddy_frontend/src/userinterface/components/ProductComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductComponent.js
@@ -12,10 +12,13 @@ export default function ProductComponent() {
             <div style={{ height: '50vh', width: '100%', background: ' #191919', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <div style={{ display: 'flex', alignItems: 'center', width: '80%', height: '100%' }}>
                        { data.map((item) => (
-                        <div style={{ height: '85%', width: '20%', background: ' black', margin: 30, borderRadius: 10, display: "flex", justifyContent: 'center' }}>
+                        <div style={{ height: '85%', width: '20%', background: ' black', margin: 30, borderRadius: 10, display: "flex", justifyContent: 'center',flexDirection:'column' }}>
                             <div style={{ display: 'flex', justifyContent: 'center', height: '70%', width: '99%', position: 'relative' }}>
                                 <img src={`${serverURL}/images/${item.image}`} style={{ width: '90%', height: '100%', marginTop: '7%' }} />
                                 <FavoriteBorderIcon style={{ fontSize: '200%', color: 'white', position: 'absolute', right: '1%', top: '2%' }} />
+                            </div>
+                            <div style={{width:'90%',height:'30%',}}>
+
                             </div>
                         </div>
                     ))}
