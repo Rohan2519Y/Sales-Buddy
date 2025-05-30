dd400803e3094bcaed4be18a03887cb7641bfc30
Rohan2519Y <rohankumar17645@gmail.com>
2025-05-30 13:06:28 +0530

    Update ProductComponent.js

--- salebuddy_frontend/src/userinterface/components/ProductComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductComponent.js
@@ -2,23 +2,26 @@ import { serverURL } from "../../backendservices/FetchNodeServices"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 export default function ProductComponent() {
     var data = [{
-        productdetailid: 1, productname: 'Apple iPhone 16', variant: '128 GB', color: 'Teal',
+        productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 512GB, Titanium Gray)', variant: '128 GB', color: 'Teal',
         image: '1ee1673c-4cac-4d62-aba2-bd9d577b3b09.png', ratings: 5, price: 78000.00, offer: 70000.00
     }]
 
     const showProducts = () => {
 
         return (
-            <div style={{ height: '50vh', width: '100%', background: ' #191919', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{ height: '55vh', width: '100%', background: ' #191919', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <div style={{ display: 'flex', alignItems: 'center', width: '80%', height: '100%' }}>
-                       { data.map((item) => (
-                        <div style={{ height: '85%', width: '20%', background: ' black', margin: 30, borderRadius: 10, display: "flex", justifyContent: 'center',flexDirection:'column' }}>
-                            <div style={{ display: 'flex', justifyContent: 'center', height: '70%', width: '99%', position: 'relative' }}>
-                                <img src={`${serverURL}/images/${item.image}`} style={{ width: '90%', height: '100%', marginTop: '7%' }} />
+                    {data.map((item) => (
+                        <div style={{ height: '100%', width: '20%', background: ' black', margin: 30, borderRadius: 10, display: "flex", justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                            <div style={{ display: 'flex', justifyContent: 'center', height: '60%', width: '99%', position: 'relative', marginBottom: '5%' }}>
+                                <img src={`${serverURL}/images/${item.image}`} width={'90%'} style={{ marginTop: '7%', position: 'absolute' }} />
                                 <FavoriteBorderIcon style={{ fontSize: '200%', color: 'white', position: 'absolute', right: '1%', top: '2%' }} />
                             </div>
-                            <div style={{width:'90%',height:'30%',}}>
+                            <div style={{ width: '100%', height: '40%', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                                <div style={{ width: '95%', color: 'white', justifyContent: 'center' }}>{item.productname}</div>
+                                <div style={{ width: '95%', color: 'white', justifyContent: 'center' , background: 'red'}}>
 
+                                </div>
                             </div>
                         </div>
                     ))}
