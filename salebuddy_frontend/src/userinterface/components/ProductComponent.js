391d7cbb768264cd57ae57e64d86800f2bb8690e
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-01 17:32:33 +0530

    Update ProductComponent.js

--- salebuddy_frontend/src/userinterface/components/ProductComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductComponent.js
@@ -9,23 +9,31 @@ export default function ProductComponent() {
 
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('md'));
-    const smallmatches = useMediaQuery(theme.breakpoints.down('sm'));
-    const [heart, setHeart] = useState('white')
+    const smatches = useMediaQuery(theme.breakpoints.down('sm'));
+    const [heart, setHeart] = useState(null)
 
     var data = [{
         productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 512GB, Titanium Gray)', variant: '128 GB', color: 'Teal',
-        image: '1f2f5fd2-9076-48c7-8075-14642a9d2989.png', ratings: 3.3, price: 78000.00, offer: 70000.00
+        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748777796/15_pro_max_p1iwhz.jpg', ratings: 3.3, price: 78000.00, offer: 70000.00
+    },
+    {
+        productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 512GB, Titanium Gray)', variant: '128 GB', color: 'Teal',
+        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778280/sam_z6_v6ubdl.png', ratings: 3.3, price: 78000.00, offer: 70000.00
+    },
+    {
+        productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 512GB, Titanium Gray)', variant: '128 GB', color: 'Teal',
+        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778565/s25_ultra_e1hcpg.png', ratings: 3.3, price: 78000.00, offer: 70000.00
     }]
 
     const showProducts = () => {
         return (
             <div style={{ height: '52vh', width: '100%', background: ' #191919', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <div style={{ display: 'flex', alignItems: 'center', width: '80%', height: '100%' }}>
-                    {data.map((item) => (
+                    {data.map((item,i) => (
                         <div style={{ height: matches ? 300 : 350, width: matches ? 170 : 220, background: ' black', margin: 30, borderRadius: 10, display: "flex", justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                            <div style={{ display: 'flex', justifyContent: 'center', height: '60%', width: '99%', position: 'relative' }}>
-                                <img src={`${serverURL}/images/${item.image}`} width={'90%'} style={{ marginTop: '2%', position: 'absolute' }} />
-                                <FavoriteBorderIcon onMouseEnter={() => { setHeart(' #00e9bf') }} onMouseLeave={() => { setHeart('white') }} style={{ fontSize: matches ? '150%' : '200%', color: heart, position: 'absolute', right: '1%', cursor: 'pointer' }} />
+                            <div style={{ display: 'flex', justifyContent: 'center', height: '60%', width: '90%', position: 'relative', alignItems: 'center' }}>
+                                <img src={item.image} width={'75%'} style={{ position: 'absolute' }} />
+                                <FavoriteBorderIcon onMouseEnter={() => setHeart(i) } onMouseLeave={() =>setHeart(null) } style={{ fontSize: matches ? '150%' : '200%', color: heart == i ? ' #00e9bf' : 'white', position: 'absolute', right: '-4%', top: '-4%', cursor: 'pointer' }} />
                             </div>
                             <div style={{ width: '95%', height: '30%', alignItems: 'center', display: 'flex', flexDirection: 'column', marginBottom: '5%' }}>
                                 <div style={{ width: '90%', color: 'white', justifyContent: 'center', marginBottom: '2%' }}>{matches ? (item.productname.length > 33 ? item.productname.substring(0, 33) + '...' : item.productname) : (item.productname.length > 40 ? item.productname.substring(0, 40) + '...' : item.productname)}</div>
