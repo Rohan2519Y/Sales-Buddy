1d585008f8d3486da90ae659a4c0e25a8b19d5f8
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-03 12:14:46 +0530

    adding title

--- salebuddy_frontend/src/userinterface/components/ProductScroller.js
+++ salebuddy_frontend/src/userinterface/components/ProductScroller.js
@@ -9,10 +9,10 @@ import { useTheme } from '@mui/material/styles';
 import { useRef } from "react";
 import ProductComponent from "./ProductComponent";
 
-export default function ProductScroller({ data }) {
+export default function ProductScroller({ data, title }) {
     const theme = useTheme();
     const md = useMediaQuery('(max-width:1200px)');
-const sm = useMediaQuery('(max-width:700px)');
+    const sm = useMediaQuery('(max-width:700px)');
     const matches = useMediaQuery(theme.breakpoints.down('md'));
     const smatches = useMediaQuery(theme.breakpoints.down('sm'));
     const landscape = useMediaQuery('(max-height: 500px) and (min-width: 600px)');
@@ -32,9 +32,12 @@ const sm = useMediaQuery('(max-width:700px)');
 
 
     return (<>
+        <div style={{ width: '100%', color: 'white', height: '10vh', display: 'flex', alignItems: 'center', fontSize: matches ? '170%' : '200%', justifyContent: 'center' }}>
+            <div style={{ width: matches?'75%':'78%' }}> {title}</div>
+        </div>
         <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ width: '9%', display: 'flex',justifyContent:'flex-end' }}><ProductScrollerBack productScroller={ref} /></div>
-            <div style={{ width: '82%', margin: '0 auto', padding: 0, position: 'relative' }}>
+            <div style={{ width: '9%', display: 'flex', justifyContent: 'flex-end' }}><ProductScrollerBack productScroller={ref} /></div>
+            <div style={{ width: smatches?'90%':'82%', margin: '0 auto', padding: 0, position: 'relative' }}>
                 <Slider ref={ref} {...settings}>
                     {data.map((item, i) => (
                         <div key={i} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: landscape ? '90vh' : smatches ? '40vh' : '52vh', width: '100%', }}  >
@@ -47,7 +50,7 @@ const sm = useMediaQuery('(max-width:700px)');
                     ))}
                 </Slider>
             </div>
-            <div style={{ width: '9%', display: 'flex'}}><ProductScrollerFront productScroller={ref} /></div>
+            <div style={{ width: '9%', display: 'flex' }}><ProductScrollerFront productScroller={ref} /></div>
         </div>
     </>);
 }
--- salebuddy_frontend/src/userinterface/screens/Home.js
+++ salebuddy_frontend/src/userinterface/screens/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
         productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 512GB, Titanium Gray)', variant: '128 GB', color: 'Teal',
         image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778280/sam_z6_v6ubdl.png', ratings: 3.3, price: 78000.00, offer: 70000.00
     },
-{
+    {
         productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 512GB, Titanium Gray)', variant: '128 GB', color: 'Teal',
         image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748777796/15_pro_max_p1iwhz.jpg', ratings: 3.3, price: 78000.00, offer: 70000.00
     },
@@ -30,7 +30,7 @@ export default function Home() {
             <MainSlider />
         </div>
         <div style={{ width: '100%', height: '100%', overflowX: "hidden", background: ' #191919' }}>
-            <ProductScroller data={data}/>
+            <ProductScroller data={data} title='Summer Special Deals' />
         </div>
         <div >
             <BrandSlider />
