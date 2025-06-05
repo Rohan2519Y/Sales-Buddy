0ab9577c836d527346c251920da297a6068174e6
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-05 13:24:02 +0530

    Update ProductComponent.js

--- salebuddy_frontend/src/userinterface/components/ProductComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductComponent.js
@@ -19,16 +19,17 @@ export default function ProductComponent({ item, i }) {
         <div style={{ height: smatches ? 270 : matches ? 330 : 370, width: '100%', background: ' black', marginLeft: '3.5%', marginRight: '3.5%', borderRadius: 10, display: "flex", justifyContent: 'center', flexDirection: 'column', alignItems: 'center', fontFamily: '"Inter", sans-serif' }}>
             <div style={{ display: 'flex', justifyContent: 'center', height: '60%', width: '90%', position: 'relative', alignItems: 'center' }}>
                 <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', justifyContent: 'center' }}><img src={item.image} style={{ objectFit: 'cover', margin: '5%' }} /></div>
-                <FavoriteBorderIcon onMouseEnter={!smatches ? () => setHeart(i) : undefined} onMouseLeave={!smatches ? () => setHeart(null) : undefined} style={{ fontSize: matches ? '150%' : '200%', color: heart == i ? ' #00e9bf' : 'white', position: 'absolute', right: '-4%', top: '-4%', cursor: landscape ? '' : smatches ? '' : 'pointer' }} />
+                <FavoriteBorderIcon onMouseEnter={!smatches ? () => setHeart(i) : undefined} onMouseLeave={!smatches ? () => setHeart(null) : undefined} style={{ fontSize: matches ? '130%' : '170%', color: heart == i ? ' #00e9bf' : 'white', position: 'absolute', right: '-4%', top: '1%', cursor: landscape ? '' : smatches ? '' : 'pointer' }} />
             </div>
             <div style={{ width: '95%', height: '30%', alignItems: 'center', display: 'flex', flexDirection: 'column', marginBottom: '5%' }}>
-                <div style={{ width: '90%', height:smatches?'75%': matches ? '55%' : '50%', color: 'white', justifyContent: 'center', marginBottom: '2%', fontSize: smatches ? '90%' : matches ? '100%' : '115%', display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden', textOverflow: 'ellipsis', lineHeight: '1.2' }}>{item.productname}...</div>
+                <div style={{ width: '90%', height: smatches ? '55%' : matches ? '50%' : '45%', color: 'white', justifyContent: 'center', marginBottom: '2%', fontSize: smatches ? '80%' : matches ? '90%' : '100%', display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden', textOverflow: 'ellipsis', lineHeight: '1.2' }}>{item.productname}...</div>
                 <div style={{ width: '90%', height: '50%', color: 'white', display: 'flex', alignItems: 'center' }}>
-                    <div style={{ fontSize: landscape ? '100%' : smatches ? '100%' : matches ? '125%' : "130%", fontWeight: 500 }}>{'\u20B9'}{item.price.toFixed(2)}</div>
-                    <div style={{ width: '100%', display: 'flex', height: '70%', justifyContent: 'center' }}><div style={{ width: '90%', fontSize: landscape ? '75%' : smatches ? '75%' : matches ? '80%' : "92%", fontWeight: 400, color: 'grey', height: '100%', display: 'flex' }}><s>{'\u20B9'}{item.offer.toFixed(2)}</s></div></div>
+                    <div style={{ fontSize: landscape ? '100%' : smatches ? '90%' : matches ? '115%' : "120%", fontWeight: 500 }}>{'\u20B9'}{item.price.toFixed(2)}</div>
+                    <div style={{ width: '100%', display: 'flex', height: '70%', justifyContent: 'center' }}><div style={{ width: '90%', fontSize: landscape ? '75%' : smatches ? '75%' : matches ? '80%' : "88%", fontWeight: 400, color: 'grey', height: '100%', display: 'flex' }}><s>{'\u20B9'}{item.offer.toFixed(2)}</s></div></div>
                 </div>
                 <div style={{ display: 'flex', width: '90%' }}>
-                    <Rating name="half-rating-read" defaultValue={item.ratings} precision={0.1} readOnly icon={<StarIcon sx={{ color: '#00e9bf' }} />} emptyIcon={<StarIcon sx={{ color: 'grey' }} />} />
+                    {smatches ? <Rating name="half-rating-read" defaultValue={item.ratings} precision={0.1} readOnly icon={<StarIcon sx={{ color: '#00e9bf' }} style={{fontSize:'95%'}}/>} emptyIcon={<StarIcon sx={{ color: 'grey' }} style={{fontSize:'95%'}} />} /> :
+                        <Rating name="half-rating-read"  defaultValue={item.ratings} precision={0.1} readOnly icon={<StarIcon sx={{ color: '#00e9bf' }} />} emptyIcon={<StarIcon sx={{ color: 'grey' }} />} />}
                 </div>
             </div>
         </div>
