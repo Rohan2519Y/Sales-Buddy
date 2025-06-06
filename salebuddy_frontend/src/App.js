1cc9be4b9752b6c136e18f36a38e4d9d3b46566b
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-06 21:48:40 +0530

    adding header in product list

--- salebuddy_frontend/src/App.js
+++ salebuddy_frontend/src/App.js
@@ -1,6 +1,7 @@
 import SignIn from "./admin/admin_login/SignIn"
 import Dashboard from "./admin/admin_login/Dashboard"
 import Home from "./userinterface/screens/Home"
+import ProductList from "./userinterface/screens/ProductList";
 import {BrowserRouter as Router,Routes,Route} from "react-router";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route element={<SignIn/>} path="/signin"/>
           <Route element={<Dashboard/>} path="/dashboard/*"/>
+          <Route element={<ProductList/>} path="/productlist"/>
           <Route element={<Home/>} path="/"/>
         </Routes>
       </Router>
--- salebuddy_frontend/src/userinterface/screens/ProductList.js
+++ salebuddy_frontend/src/userinterface/screens/ProductList.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 
-export default function ProductList(){
-    return(
-        <div style={{width:'100%',height:'100%',display:'flex'}}>
-            <div><Header/></div>
+export default function ProductList() {
+    return (
+        <div style={{ width: '100%', display: 'flex',flexDirection:'column' }}>
+            <div  >
+                <Header />
+            </div>
         </div>
-    )    
+    )
 }
