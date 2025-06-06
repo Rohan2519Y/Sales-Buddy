e394db547ca28a40d85d23825f3ad79fb7a1ac5d
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-06 21:41:49 +0530

    Create ProductList.js

---
+++ salebuddy_frontend/src/userinterface/screens/ProductList.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+export default function ProductList(){
+    return(
+        <div style={{width:'100%',height:'100%',display:'flex'}}>
+            <div><Header/></div>
+        </div>
+    )    
+}
