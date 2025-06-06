21fcc57ad1af1679f4b351ee7beb062a97c68bc4
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-06 22:12:41 +0530

    updating dropdown

--- salebuddy_frontend/src/userinterface/components/Dropdown.js
+++ salebuddy_frontend/src/userinterface/components/Dropdown.js
@@ -1,70 +1,30 @@
-import * as React from 'react';
-import {
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Checkbox,
-  ListItemText,
-  OutlinedInput
-} from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useState } from 'react';
 
-const storageOptions = [
-  { label: '256GB', count: 15 },
-  { label: '128GB', count: 4 },
-  { label: '512GB', count: 14 },
-  { label: '1TB', count: 1 }
-];
-
-export default function StorageFilter() {
-  const [selectedStorage, setSelectedStorage] = React.useState([]);
-
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setSelectedStorage(typeof value === 'string' ? value.split(',') : value);
-  };
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 200 }}>
-      <InputLabel id="storage-label" sx={{ color: '#00e9bf' }}>Internal Storage (GB)</InputLabel>
-      <Select
-        labelId="storage-label"
-        multiple
-        value={selectedStorage}
-        onChange={handleChange}
-        input={<OutlinedInput label="Internal Storage (GB)" />}
-        renderValue={(selected) => selected.join(', ')}
-        MenuProps={{
-          PaperProps: {
-            sx: {
-              bgcolor: '#1e1e1e', // dark background
-              color: 'white',
-              maxHeight: 300
-            }
-          }
-        }}
-        sx={{
-          color: '#00e9bf',
-          '& .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#00e9bf',
-          },
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#00e9bf',
-          },
-          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#00e9bf',
-          },
-        }}
-      >
-        {storageOptions.map((option) => (
-          <MenuItem key={option.label} value={option.label}>
-            <Checkbox checked={selectedStorage.indexOf(option.label) > -1} />
-            <ListItemText primary={`${option.label} (${option.count})`} />
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-  );
+export default function Dropdown() {
+    
+    const [age,setAge]=useState('')
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+    return (<>
+        <div>
+            <FormControl sx={{ m: 1, minWidth: 120,height:10 }}>
+                <Select
+                    value={age}
+                    onChange={handleChange}
+                    displayEmpty
+                >
+                    <MenuItem value="">Category</MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+    </>)
 }
--- salebuddy_frontend/src/userinterface/screens/ProductList.js
+++ salebuddy_frontend/src/userinterface/screens/ProductList.js
@@ -1,10 +1,14 @@
 import Header from "../components/Header";
+import Dropdown from '../components/Dropdown'
 
 export default function ProductList() {
     return (
         <div style={{ width: '100%', display: 'flex',flexDirection:'column' }}>
             <div  >
                 <Header />
+            </div>
+            <div>
+                <Dropdown/>
             </div>
         </div>
     )
