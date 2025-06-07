972fa67ff9ae61d463aaafcf22d324aecc2895cc
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-07 08:54:56 +0530

    Update Dropdown.js

--- salebuddy_frontend/src/userinterface/components/Dropdown.js
+++ salebuddy_frontend/src/userinterface/components/Dropdown.js
@@ -1,10 +1,17 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react';
 
+export default function CategoryDropdown() {
 
-export default function Dropdown() {    
-    
-    return (<>
-        <div>
-            
-        </div>
-    </>)
+    const [open,setOpen]=useState(false)
+  return (
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 140, height: 35, background: '#373737', borderRadius: 10,}}>
+        <button style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '90%', height: '70%', outline: 'none', background: '#393939', border: 'none', borderRadius: 8,color:' #fff'}}>
+          Category <ArrowDropDownIcon />
+        </button>
+
+      </div>
+    </div>
+  );
 }
