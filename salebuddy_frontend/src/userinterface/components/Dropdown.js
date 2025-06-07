8ba53b7d677d66bebfd946d68d039e72016c0052
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-07 08:18:59 +0530

    Update Dropdown.js

--- salebuddy_frontend/src/userinterface/components/Dropdown.js
+++ salebuddy_frontend/src/userinterface/components/Dropdown.js
@@ -1,30 +1,10 @@
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { useState } from 'react';
 
-export default function Dropdown() {
+
+export default function Dropdown() {    
     
-    const [age,setAge]=useState('')
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
     return (<>
         <div>
-            <FormControl sx={{ m: 1, minWidth: 120,height:10 }}>
-                <Select
-                    value={age}
-                    onChange={handleChange}
-                    displayEmpty
-                >
-                    <MenuItem value="">Category</MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-            </FormControl>
+            
         </div>
     </>)
 }
