be960c77cec0934e418c15d73eeb4beef9042282
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-08 21:01:58 +0530

    Update MultipleDropdown.js

--- salebuddy_frontend/src/userinterface/components/MultipleDropdown.js
+++ salebuddy_frontend/src/userinterface/components/MultipleDropdown.js
@@ -8,25 +8,26 @@ export default function MultipleDropdown() {
     const tower = ['5G', '4G', '3G', '2G']
     const discount = ['BELOW 20%', '20% TO 40%', '40% TO 60%', '60% TO 80%', '80% AND ABOVE']
     const delivery = ['EXPRESS DELIVERY', 'HOME DELIVERY']
+    
     return (<>
-        <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center',height:60 }}>
-            <div style={{ width: '80%',display:'flex',overflowX:'auto',whiteSpace:'nowrap',overflowY:'visible',scrollbarWidth:'none',WebkitOverflowScrolling: 'touch',msOverflowStyle: 'none'}}>
-                <div style={{ margin: '1%' }}>
+        <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', height: 60, overflowY: 'visible' }}>
+            <div style={{ width: '90%', display: 'flex', overflowX: 'auto',overflowY: 'visible',whiteSpace: 'nowrap',scrollbarWidth: 'none', msOverflowStyle: 'none',WebkitOverflowScrolling: 'touch' }}>
+                <div style={{ margin: '1%', minWidth: 'fit-content' }}>
                     <Dropdown data={catagories} title='Categories' />
                 </div>
-                <div style={{ margin: '1%' }}>
+                <div style={{ margin: '1%', minWidth: 'fit-content' }}>
                     <Dropdown data={storage} title='Internal Storage' />
                 </div>
-                <div style={{ margin: '1%' }}>
+                <div style={{ margin: '1%', minWidth: 'fit-content' }}>
                     <Dropdown data={brands} title='Brands' />
                 </div>
-                <div style={{ margin: '1%' }}>
+                <div style={{ margin: '1%', minWidth: 'fit-content' }}>
                     <Dropdown data={tower} title='Cellular Tower' />
                 </div>
-                <div style={{ margin: '1%' }}>
+                <div style={{ margin: '1%', minWidth: 'fit-content' }}>
                     <Dropdown data={discount} title='Discount' />
                 </div>
-                <div style={{ margin: '1%' }}>
+                <div style={{ margin: '1%', minWidth: 'fit-content' }}>
                     <Dropdown data={delivery} title='Delivery' />
                 </div>
             </div>
