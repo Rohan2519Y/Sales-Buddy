f5a0651471020d15378e289b21a861ad769e2879
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-10 20:20:50 +0530

    Update Filter.js

--- salebuddy_frontend/src/userinterface/components/ProductListComponents/Filter.js
+++ salebuddy_frontend/src/userinterface/components/ProductListComponents/Filter.js
@@ -16,8 +16,10 @@ export default function Filter({onClose}) {
         <>
             {!matches && (
                 <div onClick={handleBackdropClick} style={{position: 'fixed',top: 0,left: 0,width: '100%',height: '100%',display: 'flex',alignItems: 'center',justifyContent: 'flex-end',backgroundColor: 'rgba(0,0,0,0.5)'}}>
-                    <div style={{width: '25%',height: '100%',background: 'white',boxShadow: '-2px 0 8px rgba(0,0,0,0.2)', depthpadding: '20px', paddingoverflowY: 'auto'}}>
-                        
+                    <div style={{width: '30%',height: '100%',display:'flex',justifyContent:'center',alignItems:'center',background: 'white',boxShadow: '-2px 0 8px rgba(0,0,0,0.2)', depthpadding: '20px', paddingoverflowY: 'auto'}}>
+                        <div style={{width:'85%',background:'red',display:'flex',justifyContent:'center',height:'90%',}}>
+                            <div style={{width:'100%',background:'green',height:50}}></div>
+                        </div>
                     </div>
                 </div>
             )}
