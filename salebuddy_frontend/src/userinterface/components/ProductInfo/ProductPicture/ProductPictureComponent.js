143c43fa7ca388be89b2e7b7e891486808bed221
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-27 19:38:37 +0530

    Update ProductPictureComponent.js

--- salebuddy_frontend/src/userinterface/components/ProductInfo/ProductPicture/ProductPictureComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductInfo/ProductPicture/ProductPictureComponent.js
@@ -1,14 +1,12 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShareIcon from '@mui/icons-material/Share';
-export default function ProductPictureComponent({ media }) {
-  if (!media) return <div style={{ width: '100%', height: '80%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <div style={{ width: '80%', height: '80%', background: 'blue' }}>
-    </div>
-  </div>
-  const isVideo = media.includes('.webm') || media.includes('.mp4');
 
-  return (<>
-    <div style={{ width: '100%', height: '80%', display: 'flex',  alignItems: 'center', flexDirection: 'column' }}>
+export default function ProductPictureComponent({ media }) {
+
+  const isVideo = !!media && /\.(mp4|webm|ogg|mov|avi|flv|wmv|mkv|m4v|3gp)$/i.test(media);
+
+  return (
+    <div style={{ width: '100%', height: '80%', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
       <div style={{ width: '90%', height: '60%', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
         <div style={{ fontSize: '170%', color: 'white', position: 'absolute', right: '1%', top: '1%', cursor: 'pointer', display: 'flex' }}>
           <FavoriteBorderIcon style={{ marginRight: 20 }} />
@@ -20,8 +18,7 @@ export default function ProductPictureComponent({ media }) {
           <img src={media} style={{ maxWidth: '90%', maxHeight: '90%', objectFit: 'contain' }} />
         )}
       </div>
-      <hr style={{ width: '80%', height: '0.01%' }}></hr>
+      <hr style={{ width: '80%', height: '0.01%' }} />
     </div>
-
-  </>);
+  );
 }
