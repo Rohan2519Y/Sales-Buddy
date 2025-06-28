3051eec7536276be6ba604caa3f6695556740dc9
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-28 19:15:40 +0530

    completed productart

--- salebuddy_frontend/src/userinterface/components/CartComponent/ProductCart.js
+++ salebuddy_frontend/src/userinterface/components/CartComponent/ProductCart.js
@@ -2,8 +2,18 @@ import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
 import DoneIcon from '@mui/icons-material/Done';
 import { useState } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 export default function ProductCart({ item }) {
+
+    const theme = useTheme();
+        const md = useMediaQuery('(max-width:1300px)');
+        const sm = useMediaQuery('(max-width:700px)');
+        const matches = useMediaQuery(theme.breakpoints.down('md'));
+        const smatches = useMediaQuery(theme.breakpoints.down('sm'));
+        const landscape = useMediaQuery('(max-height: 500px) and (min-width: 600px)');
+    
 
     const [color, setColor] = useState(false)
     const [rcolor, setRcolor] = useState(false)
@@ -25,7 +35,7 @@ export default function ProductCart({ item }) {
                         <div style={{ width: '100%', height: 25 }}>
                             <Rating name="half-rating-read" defaultValue={item.ratings} precision={0.1} readOnly icon={<StarIcon sx={{ color: ' #00e9bf' }} style={{ fontSize: '70%' }} />} emptyIcon={<StarIcon sx={{ color: 'grey' }} style={{ fontSize: '70%' }} />} />
                         </div>
-                        <div style={{ width: '50%', height: 35, fontSize: '90%', }}>{item.delivery}</div>
+                        <div style={{ width: '50%', height: 35, fontSize: '85%', }}>{item.delivery}</div>
                         <div style={{ width: '100%', height: 60, display: 'flex', alignItems: 'center' }}>
                             <div onMouseEnter={() => { setColor(true) }} onMouseLeave={() => { setColor(false) }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid ', borderColor: color ? ' #00e9bf' : ' #000000', borderRadius: 10, boxSizing: 'border-box', cursor: 'pointer', marginRight: 15 }}><div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 30, marginLeft: 30, marginTop: 7, marginBottom: 7, fontSize: '90%', fontWeight: 600 }}>Move To Whislist</div></div>
                             <div onMouseEnter={() => { setRcolor(true) }} onMouseLeave={() => { setRcolor(false) }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid ', borderColor: rcolor ? ' #00e9bf' : ' #000000', borderRadius: 10, boxSizing: 'border-box', cursor: 'pointer' }}><div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 30, marginLeft: 30, marginTop: 7, marginBottom: 7, fontSize: '90%', fontWeight: 600 }}>Remove</div></div>
@@ -33,21 +43,23 @@ export default function ProductCart({ item }) {
                     </div>
                     <div style={{ width: '40%', height: '100%', display: 'flex', }}>
                         <div style={{ width: '90%', height: '100%' }}>
-                            <div style={{ width: '100%', height: 25, fontSize: '140%', fontWeight: 700, display: 'flex', justifyContent: 'flex-end' }}>{'\u20B9'}{item.offerprice}</div>
+                            <div style={{ width: '100%', height: 25, fontSize: '140%', fontWeight: 700, display: 'flex', justifyContent: 'flex-end' }}>{'\u20B9'}{item.offerprice || item.price}</div>
                             <div style={{ width: '100%', height: 30, fontSize: '90%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end' }}>(Incl. all Taxes)</div>
                             <div style={{ width: '50%', height: 0.5, background: ' #000000', justifySelf: 'flex-end', marginTop: 7, marginBottom: 10 }}></div>
-                            <div style={{ width: '100%', height: 15, fontSize: '85%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', }}><s>MRP {'\u20B9'}{item.price.toFixed(2)}</s></div>
-                            <div style={{ width: '100%', height: 15, fontSize: '75%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', color: ' #9A9A9A' }}>{'\u20B9'}{discount}</div>
-                            <div style={{ width: '50%', height: 0.5, background: ' #000000', justifySelf: 'flex-end', marginTop: 10, marginBottom: 10 }}></div>
+                            {item.offerprice && (<>
+                                <div style={{ width: '100%', height: 15, fontSize: '85%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', }}><s>MRP {'\u20B9'}{item.price.toFixed(2)}</s></div>
+                                <div style={{ width: '100%', height: 15, fontSize: '75%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', color: ' #9A9A9A' }}>{'\u20B9'}{discount}</div>
+                                <div style={{ width: '50%', height: 0.5, background: ' #000000', justifySelf: 'flex-end', marginTop: 10, marginBottom: 10 }}></div>
+                            </>)}
                             <div style={{ width: '100%', height: 25, fontSize: '100%', fontWeight: 700, display: 'flex', justifyContent: 'flex-end' }}>{'\u20B9'}6457/mo*</div>
                             <div style={{ width: '100%', height: 15, fontSize: '80%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', color: ' #088466' }}><u>EMI Options</u></div>
                         </div>
                     </div>
                 </div>
-                <div style={{ width: '100%', height: 0.5, background: ' #000000', justifySelf: 'flex-end', marginTop: 10, marginBottom: 10 }}></div>
-                <div style={{ height: '25%', width: '60%', display: 'flex',  alignItems: 'center' }}>
-                    <DoneIcon style={{display:'flex',alignSelf:'flex-start',fontSize:'140%',color:' #00e9bf',marginRight:10}}/><div style={{fontSize:'85%'}}>Buy & Get Rs.2010 off (Discount auto applied in cart) Offer Applied</div>
-                </div>
+                {item.offerapply && (<><div style={{ width: '100%', height: 0.5, background: ' #000000', justifySelf: 'flex-end', marginTop: 10, marginBottom: 10 }}></div>
+                    <div style={{ height: '25%', width: '60%', display: 'flex', alignItems: 'center' }}>
+                        <DoneIcon style={{ display: 'flex', alignSelf: 'flex-start', fontSize: '140%', color: ' #00e9bf', marginRight: 10 }} /><div style={{ fontSize: '85%' }}>{item.offerapply}</div>
+                    </div></>)}
             </div>
         </div>
     </>)
--- salebuddy_frontend/src/userinterface/components/CartComponent/ShowProductCart.js
+++ salebuddy_frontend/src/userinterface/components/CartComponent/ShowProductCart.js
@@ -1,20 +1,30 @@
 import ProductCart from "./ProductCart"
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 export default function ShowProductCart() {
 
+    const theme = useTheme();
+    const md = useMediaQuery('(max-width:1300px)');
+    const sm = useMediaQuery('(max-width:700px)');
+    const matches = useMediaQuery(theme.breakpoints.down('md'));
+    const smatches = useMediaQuery(theme.breakpoints.down('sm'));
+    const landscape = useMediaQuery('(max-height: 500px) and (min-width: 600px)');
+
+
     var data = [{
-        productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 256GB, Titanium Silverblue)', storage: '128GB', color: 'Titanium Blue', ram: '12GB',
-        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778565/s25_ultra_e1hcpg.png', ratings: 3.3, price: 78000.00, offerprice: 70000.00,
-        delivery: 'Standard Delivery by 1 July 2025 | Free'
+        productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 256GB, Titanium Silverblue)', storage: '256GB', color: 'Titanium Blue', ram: '12GB',
+        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778565/s25_ultra_e1hcpg.png', ratings: 4, price: 78000.00,
+        delivery: 'Standard Delivery by 1 July 2025 | Free', offerapply: 'Buy & Get Rs.2010 off (Discount auto applied in cart) Offer Applied'
     },
     {
-        productdetailid: 1, productname: 'SAMSUNG Galaxy S25 Ultra 5G (12GB RAM, 256GB, Titanium Silverblue)', storage: '128GB', color: 'Titanium Blue', ram: '12GB',
-        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778565/s25_ultra_e1hcpg.png', ratings: 3.3, price: 78000.00, offerprice: 70000.00,
+        productdetailid: 1, productname: 'SAMSUNG Galaxy Z6 (12GB RAM, 128GB, Titanium Silverblack)', storage: '128GB', color: 'Titanium Blue', ram: '12GB',
+        image: 'https://res.cloudinary.com/dio6iadsq/image/upload/v1748778280/sam_z6_v6ubdl.png', ratings: 3.6, price: 88000.00, offerprice: 80000.00,
         delivery: 'Standard Delivery by 1 July 2025 | Free'
     }]
 
     return (<>
-        <div style={{ display: 'flex', width: '100%', alignItems: 'flex-end',  flexDirection: 'column' }}>
+        <div style={{ display: 'flex', width: '100%', alignItems: 'flex-end', flexDirection: 'column' }}>
             {data.map((item) => (
                 <div style={{ width: '80%', height: 280, background: ' #ffffff', marginBottom: 30, display: 'flex', alignItems: 'center' }}>
                     <ProductCart item={item} />
