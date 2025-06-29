a13f8b0ed206fb58045c4180fdb5af7dd120bae9
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-29 12:24:55 +0530

    Update ProductCart.js

--- salebuddy_frontend/src/userinterface/components/CartComponent/ProductCart.js
+++ salebuddy_frontend/src/userinterface/components/CartComponent/ProductCart.js
@@ -27,8 +27,26 @@ export default function ProductCart({ item }) {
                 <div style={{ width: '40%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
                     <img src={item.image} style={{ maxWidth: '90%', maxHeight: '100%' }} />
                 </div>
-                <div style={{ width: '40%', height: '100%', display: 'flex',  }}>
-                    <div style={{ width: '100%', height: 50, fontSize: '100%', fontWeight: 550, }}>{item.productname}</div>
+                <div style={{ width: '60%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ width: '100%', height: 40, fontSize: '100%', fontWeight: 550, display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden', textOverflow: 'ellipsis', }}>{item.productname}</div>
+                    <div style={{ width: '100%', height: 25 }}>
+                        <Rating name="half-rating-read" defaultValue={item.ratings} precision={0.1} readOnly icon={<StarIcon sx={{ color: ' #00e9bf' }} style={{ fontSize: '70%' }} />} emptyIcon={<StarIcon sx={{ color: 'grey' }} style={{ fontSize: '70%' }} />} />
+                    </div>
+                    <div style={{ width: '100%', display: 'flex' }}>
+                        <div style={{ width: '40%', height: 40 }}>
+                            <div style={{ width: '100%', height: 15, fontSize: '85%', display: 'flex', }}>{'\u20B9'}{(item.offerprice ?? item.price)?.toFixed(2)}</div>
+                            <div style={{ width: '100%', height: 15, fontSize: '70%', display: 'flex' }}>(Incl. all Taxes)</div>
+                        </div>
+                        <div style={{ width: '20%', height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <div style={{ width: '20%', height: 0.5, background: ' #000000' }}></div>
+                            <div style={{ width: '60%', height: '80%', border: '1px solid #000000', borderRadius: 5, display: 'flex', justifyContent: 'center', alignItems: 'center',fontSize:'90%' }}>OR</div>
+                            <div style={{ width: '20%', height: 0.5, background: ' #000000' }}></div>
+                        </div>
+                        <div style={{ width: '40%', height: 40, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                            <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', fontWeight: 550, fontSize: '90%' }}>{'\u20B9'}6457/mo*</div>
+                            <div style={{ width: '100%', fontSize: '70%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', color: ' #088466' }}><u>EMI Options</u></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </> :
