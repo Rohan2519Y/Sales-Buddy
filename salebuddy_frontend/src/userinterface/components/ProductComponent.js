198e5f83367adbef88b1f4f3e70ed76f568a9b89
Rohan2519Y <rohankumar17645@gmail.com>
2025-06-30 17:29:30 +0530

    update in show two ads

--- salebuddy_frontend/src/userinterface/components/ProductComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductComponent.js
@@ -18,7 +18,7 @@ export default function ProductComponent({ item, i }) {
     return (<>
         <div style={{ height: smatches ? 270 : matches ? 330 : 370, width: '90%', background: ' black', marginLeft: '3.5%', marginRight: '3.5%', borderRadius: 10, display: "flex", justifyContent: 'center', flexDirection: 'column', alignItems: 'center', fontFamily: '"Inter", sans-serif' }}>
             <div style={{ display: 'flex', justifyContent: 'center', height: '60%', width: '90%', position: 'relative', alignItems: 'center' }}>
-                <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', justifyContent: 'center' }}><img src={`${serverURL}/images/${item.picture}`} style={{ width:'auto',margin: '5%' }} /></div>
+                <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', justifyContent: 'center' }}><img src={`${serverURL}/images/${item.picture}`} style={{ width: 'auto', margin: '5%' }} /></div>
                 <FavoriteBorderIcon onMouseEnter={!smatches ? () => setHeart(i) : undefined} onMouseLeave={!smatches ? () => setHeart(null) : undefined} style={{ fontSize: matches ? '130%' : '170%', color: heart == i ? ' #00e9bf' : 'white', position: 'absolute', right: '-4%', top: '1%', cursor: landscape ? '' : smatches ? '' : 'pointer' }} />
             </div>
             <div style={{ width: '95%', height: '30%', alignItems: 'center', display: 'flex', flexDirection: 'column', marginBottom: '5%' }}>
