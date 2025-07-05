156a33678d25782420961f053dc67a9722ee8475
Rohan2519Y <rohankumar17645@gmail.com>
2025-07-05 16:16:58 +0530

    Update ProductColorComponent.js

--- salebuddy_frontend/src/userinterface/components/ProductInfo/ProductDetails/ProductColorComponent.js
+++ salebuddy_frontend/src/userinterface/components/ProductInfo/ProductDetails/ProductColorComponent.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react"
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
+import { postData } from "../../../../backendservices/FetchNodeServices";
+import { useParams } from "react-router";
 
-export default function ProductColorComponent({ color, defaultColor }) {
+export default function ProductColorComponent({ productid,color, defaultColor }) {
 
     const theme = useTheme();
     const md = useMediaQuery('(max-width:1300px)');
@@ -10,6 +12,13 @@ export default function ProductColorComponent({ color, defaultColor }) {
     const matches = useMediaQuery(theme.breakpoints.down('md'));
     const smatches = useMediaQuery(theme.breakpoints.down('sm'));
     const landscape = useMediaQuery('(max-height: 500px) and (min-width: 600px)');
+
+    const params=useParams()
+    const [productData, setProductData] = useState({})
+    const fetchProductDetails = async () => {
+        const response = await postData('userinterface/userinterface_fetch_productdetails_by_id', { productid: params.productid })
+        setProductData(response.data)
+    }
 
     const [pcolor, setPcolor] = useState('')
     const handleClick = (item) => {
