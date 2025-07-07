3750945e20526324188d5d6ec36781157c2cbb0d
Rohan2519Y <rohankumar17645@gmail.com>
2025-07-07 21:04:16 +0530

    adjusting in slider

--- salebuddy_frontend/src/userinterface/components/ProductInfo/ProductPicture/VerticalArrowUp.js
+++ salebuddy_frontend/src/userinterface/components/ProductInfo/ProductPicture/VerticalArrowUp.js
@@ -11,7 +11,7 @@ export default function VerticalArrowUp({VerticalArrow}) {
      const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('md'));
     return (<>
-    {matches?<></>:<div onClick={handleForward} style={{ width: 'auto', height: '5%', cursor: 'pointer', display: 'flex',marginLeft:'17%' }} >
+    {matches?<></>:<div onClick={handleForward} style={{ width: 'auto', height: '5%', cursor: 'pointer', display: 'flex',marginLeft:'13%' }} >
         <div style={{ display: 'flex', width: '100%' }}><KeyboardArrowUpIcon style={{ width: '100%', fontSize: '300%', color: 'white' }} /></div>
     </div>}</>)
 }
--- salebuddy_frontend/src/userinterface/components/ProductInfo/ProductPicture/VerticalSlider.js
+++ salebuddy_frontend/src/userinterface/components/ProductInfo/ProductPicture/VerticalSlider.js
@@ -1,111 +1,104 @@
-import Slider from "react-slick";
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
-import { useRef } from "react";
-import VerticalArrowUp from "./VerticalArrowUp";
-import VerticalArrowDown from "./VerticalArrowDown";
-import styled from 'styled-components';
-import { useState, useEffect } from "react";
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import { serverURL } from "../../../../backendservices/FetchNodeServices";
+import Slider from "react-slick"
+import PlayCircleIcon from '@mui/icons-material/PlayCircle'
+import { useRef, useState, useEffect, useMemo } from "react"
+import VerticalArrowUp from "./VerticalArrowUp"
+import VerticalArrowDown from "./VerticalArrowDown"
+import styled from 'styled-components'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
+import { serverURL } from "../../../../backendservices/FetchNodeServices"
 
 export default function VerticalSlider({ data, onImageClick }) {
-
     const theme = useTheme();
-    const md = useMediaQuery('(max-width:1300px)');
-    const sm = useMediaQuery('(max-width:700px)');
     const matches = useMediaQuery(theme.breakpoints.down('md'));
-    const smatches = useMediaQuery(theme.breakpoints.down('sm'));
-    const landscape = useMediaQuery('(max-height: 500px) and (min-width: 600px)');
 
     const StyledSlider = styled(Slider)`
-  .slick-dots li button:before {
-    color: white;
-    opacity: 10;
-  }
+      .slick-dots li button:before {
+        color: white;
+        opacity: 10;
+      }
 
-  .slick-dots li.slick-active button:before {
-    color: #00e9bf;
-    opacity: 1;
-  }
-`;
+      .slick-dots li.slick-active button:before {
+        color: #00e9bf;
+        opacity: 1;
+      }
+
+      .thumbnail:hover {
+        border: 2px solid #00e9bf !important;
+      }
+    `;
 
     const ref = useRef()
     const [selectedImage, setSelectedImage] = useState(0)
-    const [bgcolor, setBgcolor] = useState(null)
 
-
-    const settings = {
-        dots: matches ? true : false,
+    const settings = useMemo(() => ({
+        dots: matches,
         infinite: true,
         slidesToShow: matches ? 1 : 4,
         slidesToScroll: 1,
-        vertical: matches ? false : true,
-        verticalSwiping: matches ? false : true,
+        vertical: !matches,
+        initialSlide: selectedImage,
         arrows: false,
-        initialSlide: matches ? selectedImage : ''
-    };
+    }), [matches, selectedImage])
 
-    var images = data?.picture?.split(',')
+    const images = data?.picture?.split(',') || [];
 
     useEffect(() => {
-        if (onImageClick && images?.length > 0) {
-            onImageClick(images[selectedImage]);
+        if (onImageClick && images.length > 0) {
+            onImageClick(images[selectedImage])
         }
-    }, [selectedImage, onImageClick, images]);
+    }, [selectedImage, onImageClick, images])
 
     const handleClick = (item, i) => {
-        setSelectedImage(i);
-        onImageClick(item);
-    };
+        setSelectedImage(i)
+        onImageClick?.(item)
+    }
 
     const showImages = () => {
-        return images?.map((item, i) => {
-            const isVideo = !!item && /\.(mp4|webm|ogg|mov|avi|flv|wmv|mkv|m4v|3gp)$/i.test(item);
-            const isActive = selectedImage === i || bgcolor === i;
-            return (
-                matches ?
-                    <div style={{ width: '100%', display: "flex", justifyContent: 'center', alignItems: 'center', }}>
-                        <div
-                            style={{ width: '100%', height: 400, display: "flex", justifyContent: 'center', alignItems: 'center', marginTop: '7%', borderRadius: 5, cursor: 'pointer', padding: 1 }}>
-                            {isVideo ? (<video controls src={`${serverURL}/images/${item}`} style={{ height: '100%', maxWidth: '95%' }} />
-                            ) : (<img src={`${serverURL}/images/${item}`} style={{ height: '100%', }} />)}
-                        </div>
-                    </div>
-                    :
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'red' }}>
+        return images.map((item, i) => {
+            const isVideo = !!item && /\.(mp4|webm|ogg|mov|avi|flv|wmv|mkv|m4v|3gp)$/i.test(item)
+            const isActive = selectedImage === i
+
+            return matches ? (
+                <div style={{ width: '100%', display: "flex", justifyContent: 'center', alignItems: 'center' }}>
+                    <div style={{ width: '100%', height: 400, display: "flex", justifyContent: 'center', alignItems: 'center', marginTop: '7%', borderRadius: 5, cursor: 'pointer', padding: 1 }}>
                         {isVideo ? (
-                            <div
-                                onMouseEnter={() => setBgcolor(i)}
-                                onMouseLeave={() => setBgcolor(null)}
-                                onClick={() => handleClick(item, i)}
-                                style={{ width: isActive ? 77 : 79, height: isActive ? 77 : 79, display: 'flex', justifyContent: 'center', alignItems: 'center', border: isActive ? '2px solid #00e9bf' : '1px solid #ffffff', borderRadius: '4px', marginBottom: '2px' }}>
-                                <PlayCircleIcon style={{ color: '#12daa8', fontSize: '300%', width: '100%' }} />
-                            </div>
+                            <video controls src={`${serverURL}/images/${item}`} style={{ height: '100%', maxWidth: '95%' }} />
                         ) : (
-                            <div
-                                onMouseEnter={() => setBgcolor(i)}
-                                onMouseLeave={() => setBgcolor(null)}
-                                onClick={() => handleClick(item, i)}
-                                style={{ width: isActive ? 77 : 79, height: isActive ? 77 : 79, objectFit: 'cover', border: isActive ? '2px solid #00e9bf' : '1px solid #ffffff', borderRadius: '4px', background: ' #191919', display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '2px' }}>
-                                <img style={{ maxWidth: '90%', maxHeight: '90%' }} src={`${serverURL}/images/${item}`} />
-                            </div>
+                            <img src={`${serverURL}/images/${item}`} style={{ height: '100%' }} />
                         )}
                     </div>
-            );
-        });
-    };
-
+                </div>
+            ) : (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <div
+                        onClick={() => handleClick(item, i)}
+                        className="thumbnail"
+                        style={{ width: isActive ? '77px' : '79px', height: isActive ? '77px' : '79px', display: 'flex', justifyContent: 'center', alignItems: 'center', border: isActive ? '2px solid #00e9bf' : '1px solid #ffffff', borderRadius: '4px', marginTop: '3px', cursor: 'pointer', background: '#191919' }}>
+                        {isVideo ? (
+                            <PlayCircleIcon style={{ color: '#12daa8', fontSize: '300%', width: '100%' }} />
+                        ) : (
+                            <img style={{ maxWidth: '90%', maxHeight: '90%' }} src={`${serverURL}/images/${item}`} alt="" />
+                        )}
+                    </div>
+                </div>
+            )
+        })
+    }
 
     return (
-        <div style={{ width: '100%', height: 440, display: "flex", flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ width: '100%', display: 'flex', }}><VerticalArrowUp VerticalArrow={ref} /></div>
+        <div style={{ width: '100%', height: 435, display: "flex", flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginBottom: 30 }}>
+            <div style={{ width: '100%', display: 'flex' }}>
+                <VerticalArrowUp VerticalArrow={ref} />
+            </div>
             <div style={{ width: '100%', flex: 1 }}>
                 <StyledSlider ref={ref} {...settings}>
                     {showImages()}
-                </StyledSlider >
+                </StyledSlider>
             </div>
-            <div style={{ width: '100%', display: 'flex', }}><VerticalArrowDown VerticalArrow={ref} /></div>
+            <div style={{ width: '100%', display: 'flex' }}>
+                <VerticalArrowDown VerticalArrow={ref} />
+            </div>
         </div>
     );
 }
